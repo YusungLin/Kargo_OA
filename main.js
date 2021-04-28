a4c16763256c8689962dc0864969cc6cbcddf0c5
YusungLin <linyusung1998@gmail.com>
2021-04-27 21:50:32 -0400

    Add validation check for input
    Accept input only in integer form.
    Exit if there is other form of input.

--- main.js
+++ main.js
@@ -10,10 +10,27 @@ table = {
 	'8': 'Eight',
 	'9': 'Nine'
 };
+
+badchar = ['.', 'e', 'E'];
+
+function exception(str) {
+	console.log('Unknown option argument: "' + str + '"');
+	console.log('Usage: node main.js Integer1 Integer2 ...');
+	console.log('');
+	process.exit(1);
+}
+
 input = process.argv.slice(2);
 let out = ''
 input.forEach(n => {
-	/* TODO: if n is not numeric */
+	for (let i = 0; i < badchar.length; ++i) {
+		if (n.indexOf(badchar[i]) !== -1) {
+			exception(n);
+		}
+	}
+	if (isNaN(n)) {
+		exception(n);
+	}
 	let n_str = ''
 	for (let i = 0; i < n.length; i++) {
 		n_str += table[n[i]];
@@ -21,4 +38,3 @@ input.forEach(n => {
 	out += n_str + ',';
 })
 console.log(out.slice(0, -1));
-
